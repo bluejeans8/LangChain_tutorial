4aeab63894c796173b0e376ab9e71ebac1ac9bb5
bluejeans8 <jsk0821@snu.ac.kr>
2024-01-19 21:35:32 +0900

    added linkedin.py

---
+++ icebreak/third_parties/linkein.py
@@ -0,0 +1,8 @@
+import os
+import requests
+
+def scrape_linkedin_profile(linkedin_profile_url: str):
+    """scrape information from Linkein profiles,
+    Manually scrape the information from the Linkedin profile"""
+    pass
+
